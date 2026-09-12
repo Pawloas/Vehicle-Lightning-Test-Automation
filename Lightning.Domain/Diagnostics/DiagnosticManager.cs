54bf83804151765a3671a2541d5a0286aab299e6
Pawloas <91752139+Pawloas@users.noreply.github.com>
2026-09-12 16:23:10 +0200

    Added DiagnosticManager, so each Measurement (Info) can be handled separately

--- Lightning.Domain/Diagnostics/DiagnosticManager.cs
+++ Lightning.Domain/Diagnostics/DiagnosticManager.cs
@@ -7,10 +7,22 @@ namespace Lighting.Domain.Diagnostics
 {
 	public class DiagnosticManager
 	{
-		public DiagnosticCode OverAllCode { get; set; } = DiagnosticCode.None;
-		public DiagnosticSeverity OverAllSeverity { get; set; } = DiagnosticSeverity.Info;
-		public Diagnostic<Temperature> TemperatureDiagnostic { get; set; } = new Diagnostic<Temperature>();
-		public Diagnostic<Voltage> VoltageDiagnostic { get; set; } = new Diagnostic<Voltage>();
-		public Diagnostic<Intensity> IntensityDiagnostic { get; set; } = new Diagnostic<Intensity>();
+		public Diagnostic<Temperature> TemperatureDiagnostic { get; set; } = new Diagnostic<Temperature>(new Temperature(LightingConstants.OperatingTemperature));
+		public Diagnostic<Voltage> VoltageDiagnostic { get; set; } = new Diagnostic<Voltage>(new Voltage(LightingConstants.OperatingVoltage));
+		public Diagnostic<Intensity> IntensityDiagnostic { get; set; } = new Diagnostic<Intensity>(new Intensity(LightingConstants.StandardIntensity));
+
+		private List<Diagnostic> AllDiagnostics => new List<Diagnostic> { TemperatureDiagnostic, VoltageDiagnostic, IntensityDiagnostic };
+
+
+		public DiagnosticSeverity OverallSeverity()
+		{
+			return AllDiagnostics.Max(diagnostic => diagnostic.Severity);
+		}
+
+		public List<Diagnostic> OverallDiagnostics()
+		{
+			return AllDiagnostics;
+		}
+	
 	}
 }

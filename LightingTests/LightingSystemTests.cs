b77b1795c1f3c83cf3946f0dacbb977bdc2c2455
Pawloas <91752139+Pawloas@users.noreply.github.com>
2026-09-12 19:47:41 +0200

    Added tests that check possible transitions from Off mode to Position Mode

--- LightingTests/LightingSystemTests.cs
+++ LightingTests/LightingSystemTests.cs
@@ -6,27 +6,100 @@ namespace LightingTests
 	public class LightingSystemTests
 	{
 		[Fact]
-		public void NewLightingSystem_ShouldBeOff()
+		public void ShouldBeOff()
 		{
 			LightingSystem lightingSystem = new LightingSystem();
 
 			Assert.Equal(LightMode.Off, lightingSystem.LightMode);
 		}
 
-		
 		[Fact]
-		public void NewLightingSystem_ShouldEnterPositionMode()
+		public void ShouldEnterPositionMode()
 		{
-			LightingSystem lightingSystem = new LightingSystem();
+			LightingSystem lightingSystem = new LightingSystem()
+			{
+				Voltage = LightingConstants.OperatingVoltage,
+				Temperature = LightingConstants.OperatingTemperature,
+				Intensity = LightingConstants.ParkingIntensity,
 
-			lightingSystem.Voltage = LightingConstants.OperatingVoltage;
-			lightingSystem.Temperature = LightingConstants.OperatingTemperature;
-			lightingSystem.Intensity = LightingConstants.ParkingIntensity;
-
-			lightingSystem.LightMode = LightMode.Position;
+				LightMode = LightMode.Position
+			};
 
 			Assert.Equal(LightMode.Position, lightingSystem.LightMode);
 		}
+
+		[Fact]
+		public void UnderVoltage_MustNotEnterPositionMode()
+		{
+			LightingSystem lightingSystem = new LightingSystem()
+			{
+				Voltage = LightingConstants.MinVoltage,
+				Temperature = LightingConstants.OperatingTemperature,
+				Intensity = LightingConstants.ParkingIntensity,
+				LightMode = LightMode.Position
+			};
+			Assert.Equal(LightMode.Off, lightingSystem.LightMode);
+		}
+
+		[Fact]
+		public void OverVoltage_MustNotEnterPositionMode()
+		{
+			LightingSystem lightingSystem = new LightingSystem()
+			{
+				Voltage = LightingConstants.MaxVoltage,
+				Temperature = LightingConstants.OperatingTemperature,
+				Intensity = LightingConstants.ParkingIntensity,
+				LightMode = LightMode.Position
+			};
+
+			Assert.Equal(LightMode.Off, lightingSystem.LightMode);
+		}
+
+		[Fact]
+		public void OverTemperature_MustNotEnterPositionMode()
+		{
+			LightingSystem lightingSystem = new LightingSystem()
+			{
+				Voltage = LightingConstants.OperatingVoltage,
+				Temperature = LightingConstants.MaxTemperature,
+				Intensity = LightingConstants.ParkingIntensity,
+				LightMode = LightMode.Position
+			};
+			Assert.Equal(LightMode.Off, lightingSystem.LightMode);
+		}
+
+		[Fact]
+		public void OverIntensity_MustNotEnterPositionMode()
+		{
+			LightingSystem lightingSystem = new LightingSystem()
+			{
+				Voltage = LightingConstants.OperatingVoltage,
+				Temperature = LightingConstants.OperatingTemperature,
+				Intensity = LightingConstants.MaxIntensity,
+				LightMode = LightMode.Position
+			};
+			Assert.Equal(LightMode.Off, lightingSystem.LightMode);
+		}
+
+		[Fact]
+		public void FromPositionToOffMode()
+		{
+			LightingSystem lightingSystem = new LightingSystem()
+			{
+				Voltage = LightingConstants.OperatingVoltage,
+				Temperature = LightingConstants.OperatingTemperature,
+				Intensity = LightingConstants.ParkingIntensity,
+				LightMode = LightMode.Position
+			};
+			Assert.Equal(LightMode.Position, lightingSystem.LightMode);
+
+			lightingSystem.Intensity = LightingConstants.MinIntensity;
+			lightingSystem.LightMode = LightMode.Off;
+
+			Assert.Equal(LightMode.Off, lightingSystem.LightMode);
+		}
+
+
 
 	}
 }
